eb0e8f2da6be74243a1946a59b1179be0953d844
Leon <leontaveras@gmail.com>
2012-05-25 17:08:02 -0400

    removed divs cloned by Git after error.

--- application/views/layout/main.blade.php
+++ application/views/layout/main.blade.php
@@ -6,8 +6,8 @@
 <html class="no-js" lang="en">
 <!--<![endif]-->
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 			@if (isset($title)) 
 			<title>{{$title}} | Major League Mining</title>
 			@else 
@@ -17,37 +17,24 @@
 		<meta name="author" content="">
 		<link rel="stylesheet" type="text/css" href="{{ URL::to_asset("css/style.css") }}" />
 		<script src="{{ URL::to_asset("js/libs/modernizr-2.5.3-respond-1.1.0.min.js") }}"></script>
+		
 </head>
 <body>
 <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/?locale=en">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 <header name="global header">
-<div id="logobg"><br style="clear:both" /></div>
-<div class="logoholder">
-<div class="logo">
-<h1>MAJOR LEAGUE MINING</h1>
-<a href="{{ URL::to("/") }}" title="MLM"><img src="{{ URL::to_asset("images/logo.png") }}" /></a>
-</div>
+	<div id="logobg"><br style="clear:both" /></div>
+		<div class="logoholder">
+			<div class="logo">
+				<h1>MAJOR LEAGUE MINING</h1>
+				<a href="{{ URL::to("/") }}" title="MLM"><img src="{{ URL::to_asset("images/logo.png") }}" /></a>
+			</div>
 <div id="logs">
 @if (Auth::check())
  {{ Auth::user()->username }} &bull; {{ HTML::link_to_action('account@logout', "Logout") }} 
 @else
 {{ HTML::link_to_action('account@login', "Login") }} &bull; {{ HTML::link_to_action('account@login', "Create Account") }} 
 @endif
-<div id="logobg">
-	<br style="clear:both" />
 </div>
-<div class="logoholder">
-	<div class="logo">
-		<h1>MAJOR LEAGUE MINING</h1>
-		<a href="#" title="MLM"><img src="images/logo.png" /></a>
-	</div>
-	<div id="logs">
-		 @if (Auth::check()) 
-		 {{ Auth::user()->username }} &bull; {{ HTML::link_to_action('account@logout', "Logout") }} 
-		 @else 
-		 {{ HTML::link_to_action('account@login', "Login") }} &bull; {{ HTML::link_to_action('account@register', "Create Account") }} 
-		 @endif
-	</div>
 </div>
 <nav id="menu">
 <ul>
@@ -93,7 +80,7 @@
 </script>-->
 
 <script src="{{ URL::to_asset("js/libs/jquery-1.7.2.min.js") }}"></script>
-		<script src="{{ URL::to_asset("js/libs/coin-slider.min.js") }}"></script>
+	<script src="{{ URL::to_asset("js/libs/coin-slider.min.js") }}"></script>
 		<link rel="stylesheet" type="text/css" href="{{ URL::to_asset("css/coin-slider-styles.css") }}" />
 			<script type="text/javascript">
 					$(document).ready(function() {
