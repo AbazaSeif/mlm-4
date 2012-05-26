11a4047fe447cc6a4aededc6520788449f47090c
Leon <leontaveras@gmail.com>
2012-05-26 04:50:17 -0400

    Unified all jquery plugins into plugins.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,12 @@
-﻿$('#slider').orbit({
-     animation: 'fade',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+﻿ $(window).load(function() {
+$('#slider').orbit({
+     animation: 'horizontal-slide',                  // fade, horizontal-slide, vertical-slide, horizontal-push
      animationSpeed: 800,                // how fast animtions are
      timer: true, 			 // true or false to have the timer
-     advanceSpeed: 6000, 		 // if timer is enabled, time between transitions 
+     advanceSpeed: 5000, 		 // if timer is enabled, time between transitions 
      pauseOnHover: true, 		 // if you hover pauses the slider
      startClockOnMouseOut: true, 	 // if clock should start on MouseOut
-     startClockOnMouseOutAfter: 1000, 	 // how long after MouseOut should the timer start again
+     startClockOnMouseOutAfter: 300, 	 // how long after MouseOut should the timer start again
      directionalNav: true, 		 // manual advancing directional navs
      captions: true, 			 // do you want captions?
      captionAnimation: 'slideOpen', 		 // fade, slideOpen, none
@@ -14,4 +15,5 @@
      bulletThumbs: false,		 // thumbnails for the bullets
      bulletThumbLocation: '',		 // location from this file where thumbs will be
      afterSlideChange: function(){} 	 // empty function 
+});
 });
