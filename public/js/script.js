f52923934ede17e5a66075a4d73cf878a93d6fea
Leon <leontaveras@gmail.com>
2012-05-25 21:21:43 -0400

    Moved global images to "static" folder

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,17 @@
-﻿	$(document).ready(function() {
-		$('.slider').coinslider({ 
-		width: 600, // width of slider panel
-height: 300, // height of slider panel
-spw: 8, // squares per width
-sph: 8, // squares per height
-delay: 7000, // delay between images in ms
-sDelay: 30, // delay beetwen squares in ms
-opacity: 0.7, // opacity of title and navigation
-titleSpeed: 500, // speed of title appereance in ms
-effect: 'straight', // random, swirl, rain, straight
-navigation: true, // prev next and buttons
-links : true, // show images as links
-hoverPause: true // pause on hover
-		});
-	});
-
+﻿$('#slider').orbit({
+     animation: 'fade',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+     animationSpeed: 800,                // how fast animtions are
+     timer: true, 			 // true or false to have the timer
+     advanceSpeed: 6000, 		 // if timer is enabled, time between transitions 
+     pauseOnHover: true, 		 // if you hover pauses the slider
+     startClockOnMouseOut: true, 	 // if clock should start on MouseOut
+     startClockOnMouseOutAfter: 1000, 	 // how long after MouseOut should the timer start again
+     directionalNav: true, 		 // manual advancing directional navs
+     captions: true, 			 // do you want captions?
+     captionAnimation: 'slideOpen', 		 // fade, slideOpen, none
+     captionAnimationSpeed: 800, 	 // if so how quickly should they animate in
+     bullets: false,			 // true or false to activate the bullet navigation
+     bulletThumbs: false,		 // thumbnails for the bullets
+     bulletThumbLocation: '',		 // location from this file where thumbs will be
+     afterSlideChange: function(){} 	 // empty function 
+});
