f6c9c33111d3690c68755f9ab0561bfccd8ebc03
t2t2 <taavot@gmail.com>
2012-05-27 02:01:50 +0300

    Admin - User - Saving

--- application/controllers/admin/user.php
+++ application/controllers/admin/user.php
@@ -2,15 +2,54 @@
 class Admin_User_Controller extends Admin_Controller {
 	public $restful = true;
 	
+	public function __construct() {
+		parent::__construct();
+		$this->filter("before", "csrf")->on("post")->only("edit");
+	}
+
 	public function get_index() {
 		$users = DB::table("users")->get(array("id", "username", "mc_username"));
 		return View::make('admin.users.list', array("users" => $users));
 	}
 	public function get_edit($id) {
-		$user = DB::table("users")->find($id);
+		$user = DB::table("users")->find($id); // Don't need full ORM for this
 		if(!$user) {
+			Messages::add("error", "User not found");
 			return Redirect::to_action("Admin.User");
 		}
 		return View::make('admin.users.form', array("userdata" => $user));
+	}
+	public function post_edit($id) {
+		$user = User::find($id);
+		if(!$user) {
+			Messages::add("error", "User not found");
+			return Redirect::to_action("Admin.User");
+		}
+		// Angry checkbox man!
+		if(!Input::get("admin")) {
+			Input::merge(array("admin" => 0));
+		}
+		$validation_rules = array(
+			'username'  => "required|min:2|max:16|match:/^[a-z0-9_]*$/i|unique:users,username,{$user->id}",
+			'mc_username' => "required|min:2|max:16|match:/^[a-z0-9_]*$/i|unique:users,mc_username,{$user->id}"
+		);
+		$validation = Validator::make(Input::all(), $validation_rules);
+		if($validation->passes()) {
+			// Should be safe for mass-assignment
+			$user->username = Input::get("username");
+			$user->mc_username = Input::get("mc_username");
+			$user->admin = Input::get("admin");
+			$changed = $user->get_dirty();
+			if($user->save()) {
+				Event::fire("admin", array("user", "edit", $user->id, $changed));
+				Messages::add("success", "User updated!");
+				return Redirect::to_action("admin.user");
+			} else {
+				Messages::add("error", "Failed to save");
+				return Redirect::to_action('admin.user@edit', array($id))->with_input()->with_errors($validation);
+			}
+		} else {
+			return Redirect::to_action('admin.user@edit', array($id))->with_input()->with_errors($validation);
+		}
 	}
 }
--- application/views/admin/users/form.blade.php
+++ application/views/admin/users/form.blade.php
@@ -3,13 +3,19 @@
 @section('content')
 <div class="content">
 	{{ Form::open() }}
+		{{ Form::token() }}
+		@if (isset($errors))
+			@foreach ($errors->all('<p>:message</p>') as $error)
+				{{ $error }}
+			@endforeach
+		@endif
 		ID: {{ $userdata->id }}<br />
 		{{ Form::label("username", "Username") }}
-		{{ Form::text("username", $userdata->username) }}<br />
+		{{ Form::text("username", Input::old("username", $userdata->username)) }}<br />
 		{{ Form::label("mc_username", "Minecraft Username") }}
-		{{ Form::text("mc_username", $userdata->mc_username) }}<br />
+		{{ Form::text("mc_username", Input::old("mc_username", $userdata->mc_username)) }}<br />
 		{{ Form::label("admin", "Admin") }}
-		{{ Form::checkbox("admin", 1, $userdata->admin )}}<br />
+		{{ Form::checkbox("admin", 1, Input::old("admin", $userdata->admin)) }}<br />
 		{{ Form::submit("Submit", array("class" => "btn-primary")) }}
 	{{ Form::close() }}
 </div>
