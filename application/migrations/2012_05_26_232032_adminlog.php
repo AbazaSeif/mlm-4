3e7ad7702f243fb846582a671383a8f8e0504b1b
t2t2 <taavot@gmail.com>
2012-05-27 02:32:05 +0300

    !MIGRATE! Admin log database

---
+++ application/migrations/2012_05_26_232032_adminlog.php
@@ -0,0 +1,35 @@
+<?php
+
+class Adminlog {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create("adminlog", function($table) {
+			$table->increments("id");
+			$table->integer("user_id");
+			$table->string("module");
+			$table->string("action");
+			$table->integer("target")->nullable();
+			$table->text("text");
+			$table->timestamps();
+
+			$table->foreign("user_id")->references("id")->on("users")->on_delete('set null')->on_update("cascade");
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop("adminlog");
+	}
+
+}
