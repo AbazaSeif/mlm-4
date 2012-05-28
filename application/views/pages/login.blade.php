65d612760cab214bccc4feb3c38d615681d48406
Leon <leontaveras@gmail.com>
2012-05-28 12:54:46 -0400

    Updated login images to assets

--- application/views/pages/login.blade.php
+++ application/views/pages/login.blade.php
@@ -5,20 +5,20 @@
 	    <div id="login">
 		{{ Form::open("account/login" , 'POST', array('class' => 'openid')) }} 
   <div><ul class="providers"> 
-  <li class="openid" title="OpenID"><img src="http://mlm.dev/public/images/login/openidW.png" alt="icon" /> 
+  <li class="openid" title="OpenID"><img src="{{ URL::to_asset("images/login/openidW.png") }}" alt="icon" /> 
   <span><strong>http://{your-openid-url}</strong></span></li> 
   <li class="direct" title="Google"> 
-		<img src="http://mlm.dev/public/images/login/googleW.png" alt="icon" /><span>https://www.google.com/accounts/o8/id</span></li> 
+		<img src="{{ URL::to_asset("images/login/googleW.png") }}" alt="icon" /><span>https://www.google.com/accounts/o8/id</span></li> 
   <li class="direct" title="Yahoo"> 
-		<img src="http://mlm.dev/public/images/login/yahooW.png" alt="icon" /><span>http://yahoo.com/</span></li> 
+		<img src="{{ URL::to_asset("images/login/yahooW.png") }}" alt="icon" /><span>http://yahoo.com/</span></li> 
   <li class="username" title="MyOpenID user name"> 
-		<img src="http://mlm.dev/public/images/login/myopenid.png" alt="icon" /><span>http://<strong>username</strong>.myopenid.com/</span></li> 
+		<img src="{{ URL::to_asset("images/login/myopenid.png") }}" alt="icon" /><span>http://<strong>username</strong>.myopenid.com/</span></li> 
   <li class="username" title="Wordpress blog name"> 
-		<img src="http://mlm.dev/public/images/login/wordpress.png" alt="icon" /><span>http://<strong>username</strong>.wordpress.com</span></li> 
+		<img src="{{ URL::to_asset("images/login/wordpress.png") }}" alt="icon" /><span>http://<strong>username</strong>.wordpress.com</span></li> 
   <li class="username" title="ClaimID user name"> 
-		<img src="http://mlm.dev/public/images/login/claimid.png" alt="icon" /><span>http://claimid.com/<strong>username</strong></span></li>  
+		<img src="{{ URL::to_asset("images/login/claimid.png") }}" alt="icon" /><span>http://claimid.com/<strong>username</strong></span></li>  
   <li class="username" title="Verisign user name"> 
-		<img src="http://mlm.dev/public/images/login/verisign.png" alt="icon" /><span>http://<strong>username</strong>.pip.verisignlabs.com/</span></li> 
+		<img src="{{ URL::to_asset("images/login/verisign.png") }}" alt="icon" /><span>http://<strong>username</strong>.pip.verisignlabs.com/</span></li> 
   </ul></div> 
   <fieldset> 
   <label for="openid_username">Enter your <span>Provider user name</span></label> 
