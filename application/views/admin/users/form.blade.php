71869b32ad2ce1239144e32c143b73f5a41406f3
Leon <leontaveras@gmail.com>
2012-05-29 18:22:00 -0400

    User edit form styled

--- application/views/admin/users/form.blade.php
+++ application/views/admin/users/form.blade.php
@@ -3,21 +3,40 @@
 @section('content')
 @parent
 <div class="content">
-	{{ Form::open("" , 'POST', array('class' => 'form-horizontal')) }} 
+{{ Form::open("" , 'POST', array('class' => 'form-horizontal')) }} 
+	<fieldset>
 		{{ Form::token() }}
 		@if (isset($errors))
 			@foreach ($errors->all('<p>:message</p>') as $error)
 				{{ $error }}
 			@endforeach
 		@endif
-		ID: {{ $userdata->id }}<br />
-		{{ Form::label("username", "Username") }}
-		{{ Form::text("username", Input::old("username", $userdata->username)) }}<br />
-		{{ Form::label("mc_username", "Minecraft Username") }}
-		{{ Form::text("mc_username", Input::old("mc_username", $userdata->mc_username)) }}<br />
-		{{ Form::label("admin", "Admin") }}
-		{{ Form::checkbox("admin", 1, Input::old("admin", $userdata->admin)) }}<br />
+<div class="control-group">
+		{{ Form::label("username" ,"Username", array('class' => 'control-label')) }}
+	<div class="controls">
+		{{ Form::text("username", Input::old("username", $userdata->username), array('class' => 'input-large')) }}
+	</div>
+</div>
+<div class="control-group">
+		{{ Form::label("mc_username", "Minecraft Username", array('class' => 'control-label')) }}
+	<div class="controls">
+		{{ Form::text("mc_username",  Input::old("mc_username", $userdata->mc_username), array('class' => 'input-large')) }}
+</div>
+</div>
+<div class="control-group">
+      {{ Form::label("admin", "Admin", array('class' => 'control-label')) }}
+	<div class="controls">
+		{{ Form::checkbox("admin", 1, Input::old("admin", $userdata->admin), array('class' => 'checkbox')) }}
+</div>
+</div>
+<div class="form-actions">
 		{{ Form::submit("Save changes", array("class" => "btn btn-primary")) }}
-	{{ Form::close() }}
+    <div class="right">
+		{{ Form::submit("Ban user", array("class" => "btn btn-warning")) }}
+		{{ Form::submit("Delete user", array("class" => "btn btn-danger")) }}
+		</div>
+</div>
+	</fieldset>
+{{ Form::close() }}
 </div>
 @endsection
