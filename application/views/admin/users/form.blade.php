7ca478e37273a42f5122f893b571030981b48b4f
t2t2 <taavot@gmail.com>
2012-05-30 20:22:41 +0300

    Admin users edit form: Use bootsparks to display errors on same line and easier code

--- application/views/admin/users/form.blade.php
+++ application/views/admin/users/form.blade.php
@@ -6,36 +6,12 @@
 {{ Form::open(null , 'POST', array('class' => 'form-horizontal')) }} 
 	<fieldset>
 		{{ Form::token() }}
-		@if (isset($errors))
-			@foreach ($errors->all('<p>:message</p>') as $error)
-				{{ $error }}
-			@endforeach
-		@endif
-<div class="control-group">
-		{{ Form::label("username" ,"Username", array('class' => 'control-label')) }}
-	<div class="controls">
-		{{ Form::text("username", Input::old("username", $userdata->username), array('class' => 'input-large')) }}
-	</div>
-</div>
-<div class="control-group">
-		{{ Form::label("mc_username", "Minecraft Username", array('class' => 'control-label')) }}
-	<div class="controls">
-		{{ Form::text("mc_username",  Input::old("mc_username", $userdata->mc_username), array('class' => 'input-large')) }}
-</div>
-</div>
-<div class="control-group">
-      {{ Form::label("admin", "Admin", array('class' => 'control-label')) }}
-	<div class="controls">
-		{{ Form::checkbox("admin", 1, Input::old("admin", $userdata->admin), array('class' => 'checkbox')) }}
-</div>
-</div>
-<div class="form-actions">
-		{{ Form::submit("Save changes", array("class" => "btn btn-primary")) }}
-    <div class="right">
-		{{ Form::submit("Ban user", array("class" => "btn btn-warning")) }}
-		{{ Form::submit("Delete user", array("class" => "btn btn-danger")) }}
-		</div>
-</div>
+
+		{{ Form::field("text", "username", "Username", array(Input::old("username", $userdata->username), array('class' => 'input-large')), array('error' => $errors->first('username'))) }}
+		{{ Form::field("text", "mc_username", "Minecraft Username", array(Input::old("mc_username", $userdata->mc_username), array('class' => 'input-large')), array("error" => $errors->first("mc_username"))) }}
+		{{ Form::field("checkbox", "admin", "Admin", array(1, Input::old("admin", $userdata->admin), array('class' => 'checkbox'))) }}
+
+		{{ Form::actions(array( Form::submit("Save changes", array("class" => "btn btn-primary")), "<div class=\"right\">", Form::submit("Ban user", array("class" => "btn btn-warning")), "</div>" )) }}
 	</fieldset>
 {{ Form::close() }}
 </div>
