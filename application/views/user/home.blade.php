11d6ce28414c0b50cc7dec32b2f757f0e7cb7a91
Daniel Swann <swann.dp@gmail.com>
2012-05-30 08:26:03 -0400

    Changed Minotar link to use mc_username
    Also fixed indenting, since there were somehow spaces instead of tabs,
    but in the exact number required. o.0

--- application/views/user/home.blade.php
+++ application/views/user/home.blade.php
@@ -6,22 +6,21 @@
 	<div id="heading">
 		<div class="well">
 			<a href="http://minecraft.net/profile" class="thumbnail" target="_blank" title="Change your skin...">
-            <img src="http://minotar.net/helm/{{$user->username}}/60.png" alt="" />
+            	<img src="http://minotar.net/helm/{{$user->mc_username}}/60.png" alt="" />
 			</a>
-		<h2>{{$user->username}}</h2>
-		<p>Description, Tagline?</p>
+			<h2>{{$user->username}}</h2>
+			<p>Description, Tagline?</p>
 		</div>
 	</div>
-<div class="container">
-  <div class="row">
-    <div class="span4">
-      <h3>Sidebar</h3>
-    </div>
-    <div class="span8">
-      <h3>Content</h3>
-    </div>
-  </div>
-</div>
-
+	<div class="container">
+		<div class="row">
+			<div class="span4">
+				<h3>Sidebar</h3>
+			</div>
+			<div class="span8">
+				<h3>Content</h3>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
