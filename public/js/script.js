3fcfda4fbf7f5a8569dfa74b79c577e2bee330eb
Leon <leontaveras@gmail.com>
2012-06-03 16:18:13 -0400

    CSS stylesheet now well organized

--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,7 @@
 	home: {
 		init: function () {
 			$('#slider').orbit({
-				animation: 'horizontal-slide',             // fade, horizontal-slide, vertical-slide, horizontal-push
+				animation: 'horizontal-push',             // fade, horizontal-slide, vertical-slide, horizontal-push
 				animationSpeed: 800,                // how fast animtions are
 				timer: true, 			 // true or false to have the timer
 				advanceSpeed: 5000, 		 // if timer is enabled, time between transitions 
