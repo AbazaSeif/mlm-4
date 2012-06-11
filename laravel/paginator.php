ce7126ba01944b50e86552123256114c778d2325
Leon <leontaveras@gmail.com>
2012-06-11 03:08:48 -0400

    More styles changes to news pages

--- laravel/paginator.php
+++ laravel/paginator.php
@@ -65,7 +65,7 @@ class Paginator {
 	 *
 	 * @var string
 	 */
-	protected $dots = '<span class="dots">...</span>';
+	protected $dots = '<a class="dots">...</a>';
 
 	/**
 	 * Create a new Paginator instance.
@@ -300,7 +300,7 @@ class Paginator {
 		// the "first" element should be a span instead of a link.
 		if ($disabled($this->page, $this->last))
 		{
-			return HTML::span($text, array('class' => "{$class} disabled"));
+			return HTML::link('' ,"{$text}", array('class' => "{$class} disabled"));
 		}
 		else
 		{
@@ -349,7 +349,7 @@ class Paginator {
 		{
 			if ($this->page == $page)
 			{
-				$pages[] = HTML::span($page, array('class' => 'current'));
+				$pages[] = HTML::link('',"{$page}", array('class' => 'active'));
 			}
 			else
 			{
