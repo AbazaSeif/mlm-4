cfedee6f6728fa2ba4da8131ccf00b5312ea8416
Leon <leontaveras@gmail.com>
2012-06-16 04:08:50 -0400

    Revamp of User profile

--- application/views/layout/user.blade.php
+++ application/views/layout/user.blade.php
@@ -1,11 +1,12 @@
 @layout('layout.main')
 
 @section('content')
+@if (Auth::check())
 	<nav id="pagemenu">
 		<ul class="nav nav-tabs">
 			<li>{{ HTML::link('user', 'Your Profile'); }}</li>
 			<li>{{ HTML::link("account", "Edit Profile") }}</li> 
-			<li>{{ HTML::link("user/avatar", "Change your avatar") }}</li>
 		</ul>
 	</nav>
+@endif
 @endsection
--- application/views/user/home.blade.php
+++ application/views/user/home.blade.php
@@ -2,30 +2,59 @@
 
 @section('content')
 @parent
-<div id="content" class="profile">
-	<div id="heading">
-		<div class="well">
-			<a href="http://minecraft.net/profile" class="thumbnail" target="_blank" title="Change your skin...">
-            	<img src="http://minotar.net/helm/{{$user->mc_username}}/60.png" alt="" />
-			</a>
-			<h2>{{$user->username}}</h2>
-			<p>Minecrafter</p>
-		</div>
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="span4">
-				<h3>Information</h3>
-				<ul>
-				<li><h2>Team</h2></li>
-				<li><h2>Rank</h2></li>
-				<li><h2>Comments</h2></li>
-				</ul>
+<div id="content" class="profile clearfix">
+	<div id="left">
+			<div id="vcard" class="clearfix">
+				<div class="pic">
+					<a href="http://minecraft.net/profile" target="_blank" title="Change your skin..."><img src="http://minotar.net/helm/{{$user->mc_username}}/150.png" alt="avatar" /></a>
+				</div>
+				<div class="data">
+					<h1>{{$user->username}}</h1>
+					<h3><i class="flag flag-us"></i>United States</h3>
+					<h4><a href="{{$user->webzone}}">http://example.com</a></h4>
+					<div class="user-rank developer" title="MLM Developer"></div>
+					<ul class="numbers clearfix">
+						<li>Comments<strong>888</strong></li>
+						<li>Posts<strong>888</strong></li>
+						<li>Rank<strong>888</strong></li>
+					</ul>
+				</div>
 			</div>
-			<div class="span8">
-				<h3>Recent updates</h3>
-			</div>
+
+	<div class="big-info clearfix">
+		<div class="title clearfix">
+			<h2>TL;DR info for {{$user->username}}</h2>
 		</div>
+		<ul>
+		<li>
+			<label>Instant Messaging</label>
+			<strong>Skype</strong>
+		</li>
+		<li>
+			<label>Website</label>
+			<strong><a href="http://utopicme.com" rel="nofollow">http://utopicme.com</a></strong>
+		</li>
+		<li>
+			<label>Member since</label>
+			<strong>{{ HTML::entities(date("F j Y", strtotime($user->created_at))) }}</strong>
+		</li>
+		<li class="sep"><h4>Separator</h4></li>
+		<li>
+			<label>Name</label>
+			<strong>Content</strong>
+		</li>
+		</ul>
 	</div>
+</div>
+		<aside id="right">
+			<section class="sidecontent">
+				<div class="head"><h1>Team</h1></div>
+				<div class="widget">
+					<div class="badgeCount">
+						<a href="#"><img src="http://minotar.net/helm/t2t2t/50.png" /> ButCuba</a>
+					</div>
+				</div>
+			</section>
+		</aside>
 </div>
 @endsection
