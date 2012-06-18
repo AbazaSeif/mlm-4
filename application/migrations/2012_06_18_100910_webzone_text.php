d9b00e77f10e12de453d316e0c653e5df042720e
t2t2 <taavot@gmail.com>
2012-06-18 13:11:15 +0300

    !MIGRATE! Change webzone to text to allow longer url's than 200

---
+++ application/migrations/2012_06_18_100910_webzone_text.php
@@ -0,0 +1,25 @@
+<?php
+
+class Webzone_Text {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::query("ALTER TABLE `profiles` CHANGE `webzone` `webzone` text COLLATE 'utf8_general_ci' NOT NULL AFTER `youtube`, COMMENT='' REMOVE PARTITIONING;");
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::query("ALTER TABLE `profiles` CHANGE `webzone` `webzone` varchar(200) COLLATE 'utf8_general_ci' NOT NULL AFTER `youtube`, COMMENT='' REMOVE PARTITIONING;");
+	}
+
+}
