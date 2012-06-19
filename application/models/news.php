87a87ac50877e779ca4329f4000b9965b402e434
t2t2 <taavot@gmail.com>
2012-06-19 17:39:16 +0300

    Slugfield for news, forgot to commit this

--- application/models/news.php
+++ application/models/news.php
@@ -3,7 +3,8 @@ class News extends Eloquent {
 	public static $table = "news";
 
 	public static $timestamps = true;
-	
+	public static $slugfield = "title";
+
 	public function user() {
 		return $this->belongs_to("User");
 	}
