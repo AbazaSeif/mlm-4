463156003cf5e51d49f66a27005c11a41670427d
Leon <leontaveras@gmail.com>
2012-06-19 00:20:26 -0400

    Registration page compretely redone
    @t2t2 this needs to be configured to enter info into db

--- application/views/pages/register.blade.php
+++ application/views/pages/register.blade.php
@@ -1,19 +1,29 @@
 @layout('layout.main')
-
+<?php $countries = require path("app")."countries.php" ?>
 
 @section('content')
 	<div id="content">
-		{{ Form::open("account/register") }}
+<div class="titlebar clearfix center">
+	<h1>Welcome To Major League Minining!</h1>
+	<h3>Please take a few seconds to setup your profile</h3>
+</div>
+		{{ Form::open("account/register", "POST", array('class' => 'form-horizontal')) }}
 		@if (isset($errors))
 			@foreach ($errors->all('<p>:message</p>') as $error)
 				{{ $error }}
 			@endforeach
 		@endif
-		{{ Form::label("username", "Username") }}
-		{{ Form::text("username", Input::old('username')) }}
-		{{ Form::label("mc_username", "Minecraft Username") }}
-		{{ Form::text("mc_username", Input::old('mc_username')) }}
-		{{ Form::submit("Register!", array('class' => 'btn-primary')) }}
+		{{ Form::field("text", "username", "MLM Username", array(Input::old("username"), array('class' => 'input-large')),array('help' => 'You wont be able to change this later, choose wisely')) }}
+		{{ Form::field("text", "mc_username", "Minecraft Username", array(Input::old("mc_username"), array('class' => 'input-large')), array('help' => 'CaSe SeNsItIvE','error' => $errors->first('mc_username'))) }}
+		{{ Form::field("select", "country", "Country", array($countries, Input::old("country"), array('class' => 'input')), array('error' => $errors->first('country'))) }}
+		{{ Form::field("text", "reddit", "Reddit Username", array(Input::old("reddit"), array('class' => 'input-large')), array('error' => $errors->first('reddit'))) }}
+		{{ Form::field("text", "twitter", "Twitter Username", array(Input::old("twitter"), array('class' => 'input-large')), array('error' => $errors->first('twitter'))) }}
+		{{ Form::field("text", "youtube", "YouTube Username", array(Input::old("youtube"), array('class' => 'input-large')), array('error' => $errors->first('youtube'))) }}
+		{{ Form::field("text", "webzone", "Homepage", array(Input::old("webzone"), array('class' => 'input-large')), array('error' => $errors->first('webzone'))) }}
+		<div class="form-actions">
+		<button class="btn btn-primary btn-large" type="submit">Create account!</button>
+		<p class="disclaimer">By creating an account in Major League Mining, you agree to the <a href="/tos" target="_blank">Terms of Service</a></p>
+		</div>
 		{{ Form::close() }}
 	</div>
 @endsection
