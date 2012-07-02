e013d4993c9dd86ba0642e681047e50358863672
Leon <leontaveras@gmail.com>
2012-07-02 12:50:20 -0400

    Updates to news, including global sidebar

---
+++ application/views/news/sidebar.blade.php
@@ -0,0 +1,50 @@
+<aside id="sidebar">
+<ul>
+<li>
+	<div id="search" >
+		<form method="get" action="#" class="nobg">
+				<div class="input-append">
+                <input class="sbar" size="16" type="text"><button class="btn btn-primary" type="button"><i class="icon-search icon-white"></i></button>
+              </div>
+		</form>
+	</div>
+</li>
+<li>
+	<header><h1>Sidebar Header</h1></header>
+	<p>Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.</p>
+</li>
+<li>
+	<header><h1>Categories</h1></header>
+	<ul>
+		<li><a href="#">Aliquam libero</a></li>
+		<li><a href="#">Consectetuer adipiscing elit</a></li>
+		<li><a href="#">Metus aliquam pellentesque</a></li>
+		<li><a href="#">Suspendisse iaculis mauris</a></li>
+		<li><a href="#">Urnanet non molestie semper</a></li>
+		<li><a href="#">Proin gravida orci porttitor</a></li>
+	</ul>
+</li>
+<li>
+	<header><h1>Archives</h1></header>
+	<ul>
+		<li><a href="#">Aliquam libero</a></li>
+		<li><a href="#">Consectetuer adipiscing elit</a></li>
+		<li><a href="#">Metus aliquam pellentesque</a></li>
+		<li><a href="#">Suspendisse iaculis mauris</a></li>
+		<li><a href="#">Urnanet non molestie semper</a></li>
+		<li><a href="#">Proin gravida orci porttitor</a></li>
+	</ul>
+</li>
+<li>
+	<header><h1>Archives</h1></header>
+	<ul>
+		<li><a href="#">Aliquam libero</a></li>
+		<li><a href="#">Consectetuer adipiscing elit</a></li>
+		<li><a href="#">Metus aliquam pellentesque</a></li>
+		<li><a href="#">Suspendisse iaculis mauris</a></li>
+		<li><a href="#">Urnanet non molestie semper</a></li>
+		<li><a href="#">Proin gravida orci porttitor</a></li>
+	</ul>
+</li>
+</ul>
+</aside>
--- application/views/news/view.blade.php
+++ application/views/news/view.blade.php
@@ -1,22 +1,31 @@
 @layout("layout.main")
 
 @section("content")
-<div id="content" class="news">
+<div id="content" class="news clearfix">
 @if(!$article->published)
 		<div class="alert">
 			<h4 class="alert-heading">Not yet published!</h4>
 			<p>While you as an admin can view this, other users can't</p>
 		</div>
 	@endif
-<article class="post single">
-	<h2 class="title">{{ e($article->title) }}</h2>
-		<p class="meta">
-		<span class="date">{{ HTML::entities(date("F j, Y g:ia", strtotime($article->created_at))) }}</span><span class="posted">By <a href="#">{{ $article->user->username }}</a></span>
-		</p>
-		<div>{{ HTML::image($article->image->file_original, "image") }}</div>
-<div class="entry">
-		{{ $article->content }}
-</div>
+<article class="post single"> 
+		<section class="post-image"> 
+		{{ HTML::image($article->image->file_large, "Image") }}
+		</section> 
+	<div class="post-content">
+	<div class="post-info">
+		<span class="date"><p class="bold">{{ HTML::entities(date("M j", strtotime($article->created_at))) }}</p>{{ HTML::entities(date("Y", strtotime($article->created_at))) }}<br>{{ HTML::entities(date("g:ia T", strtotime($article->created_at))) }}</span>
+		<span class="comments-nr"><a href="#"><p class="bold">888</p> comments</a></span>
+		<p><span>By</span> <a href="/user/{{ $article->user->username }}" title="{{ $article->user->username }}'s Profile" rel="author">{{ $article->user->username }}</a></p>
+		<p><span>In</span> <a href="/news/category/" title="View all posts in CATEGORY" rel="category tag">CATEGORY</a></p>
+	</div>
+		<div class="post-entry"> 
+		<h1 class="title">{{ e($article->title) }}</h1> 
+		{{ nl2br(e($article->summary)) }}
+		{{ HTML::link_to_action("news@view", "Read More &raquo;" , array($article->id, $article->slug), array("class" => "more-link", )) }}
+	</div> 
+	</div>
 </article>
+@include("news.sidebar")	
 </div>
 @endsection
