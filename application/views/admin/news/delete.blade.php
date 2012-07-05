fbd0547a28bae801d421db6a5526e5a7b514674b
Leon <leontaveras@gmail.com>
2012-07-04 21:02:14 -0400

    Style fixes, changed colors to HEX

--- application/views/admin/news/delete.blade.php
+++ application/views/admin/news/delete.blade.php
@@ -4,11 +4,11 @@
 @parent
 <div id="content">
 <div class="titlebar clearfix">
-	<h2>Delete article</h2>
+	<h2>Deleting article: <strong>{{ e($newsitem->title) }}</strong></h2>
 </div>
 	{{ Form::open(null, 'POST', array('class' => 'xpadding')) }}
 	{{ Form::token() }}
-	<p>Are you sure you want to delete <strong>{{ e($newsitem->title) }}</strong>?</p>
+	<p>Are you sure you want to delete <strong>{{ e($newsitem->title) }}</strong>? (Cannot be undone!)</p>
 	{{ Form::submit("Delete", array("class" => "btn btn-danger")) }}
 	{{ HTML::link_to_action("admin.news", "Back", array(), array("class" => "btn")) }}
 	{{ Form::close() }}
