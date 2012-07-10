f9ed88a8c33eb20cce02d355bcf4e57b4ef592ac
Leon <leontaveras@gmail.com>
2012-07-10 15:28:00 -0400

    Initial comments styling

--- application/views/news/list.blade.php
+++ application/views/news/list.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div id="content" class="news clearfix">
 <div class="titlebar clearfix">
-		<h1>The News</h1>
-	</div>
+	<h1>The News</h1>
+</div>
 <div class="articles clearfix">
 	@foreach($newslist->results as $article)
-	<article class="post"> 
+	<div class="post"> 
 		<section class="post-image"> 
 		{{ HTML::image($article->image->file_large, "Image") }}
 		</section> 
 	<div class="post-content">
 	<div class="post-info">
 		<span class="date"><p class="bold">{{ HTML::entities(date("M j", strtotime($article->created_at))) }}</p>{{ HTML::entities(date("Y", strtotime($article->created_at))) }}<br>{{ HTML::entities(date("g:ia T", strtotime($article->created_at))) }}</span>
-		<span class="comments-nr"><a href="#"><p class="bold">{{ $article->comment_count }}</p> {{ Str::plural('comment', $article->comment_count) }}</a></span>
+		<span class="comments-nr"><a href="{{ URL::to_action("news@view", array($article->id, $article->slug)) }}#comments"><p class="bold">{{ $article->comment_count }}</p> {{ Str::plural('comment', $article->comment_count) }}</a></span>
 		<p><span>By</span> <a href="/user/{{ $article->user->username }}" title="{{ $article->user->username }}'s Profile" rel="author">{{ $article->user->username }}</a></p>
 		<p><span>In</span> <a href="/news/category/" title="View all posts in CATEGORY" rel="category tag">CATEGORY</a></p>
 	</div>
@@ -24,7 +24,7 @@
 		{{ HTML::link_to_action("news@view", "Read More &raquo;" , array($article->id, $article->slug), array("class" => "more-link", )) }}
 	</div> 
 	</div>
-</article>
+</div>
 @endforeach
 <?php
 // For Leon's lazyness
--- application/views/news/view.blade.php
+++ application/views/news/view.blade.php
@@ -8,39 +8,61 @@
 			<p>While you as an admin can view this, other users can't</p>
 		</div>
 	@endif
-<article class="post single"> 
+<div class="post single"> 
 		<section class="post-image"> 
 		{{ HTML::image($article->image->file_large, "Image") }}
 		</section> 
 	<div class="post-content clearfix">
 	<div class="post-info">
 		<span class="date"><p class="bold">{{ date("M j", strtotime($article->created_at)) }}</p>{{ date("Y", strtotime($article->created_at)) }}<br>{{ date("g:ia T", strtotime($article->created_at)) }}</span>
-		<span class="comments-nr"><a href="#"><p class="bold">{{ $article->comment_count }}</p> {{ Str::plural('comment', $article->comment_count) }}</a></span>
+		<span class="comments-nr"><a href="#comments"><p class="bold">{{ $article->comment_count }}</p>{{ Str::plural('comment', $article->comment_count) }}</a></span>
 		<p><span>By</span> <a href="/user/{{ $article->user->username }}" title="{{ $article->user->username }}'s Profile" rel="author">{{ $article->user->username }}</a></p>
 		<p><span>In</span> <a href="/news/category/" title="View all posts in CATEGORY" rel="category tag">CATEGORY</a></p>
 	</div>
 		<div class="post-entry"> 
 		<h1 class="title">{{ e($article->title) }}</h1> 
-		{{ nl2br(e($article->summary)) }}
 		{{ $article->content }}
 	</div> 
 	</div>
-	<h3>Comments ({{ $article->comment_count }})</h3>
+
+<div id="comments">
+	@if( $article->comment_count == 0)
+	<h2>No Comments</h2>
+	@else
+	<div class="titlebar clearfix">
+	<h2><b>{{ $article->comment_count }}</b> Comments</h2>
+	</div>
+	@endif					
+<ul id="commentlist">
 	@foreach($article->comments as $comment)
-		{{ $comment->user->username }} {{ $comment->created_at }}<br />
-		{{ $comment->html }}
+	<li>
+		<div class="comment">					
+			<div id="comment{{$comment->id}}">
+				<img class="avatar" src="http://minotar.net/helm/{{$comment->user->mc_username}}/50.png" alt="{{ $comment->user->username }}'s skin" title="{{ $comment->user->username }}'s skin">
+				<a href="{{ URL::to_action("news@view", array($article->id, $article->slug)) }}#comment{{$comment->id}}">{{ date("M j,Y g:ia", strtotime($comment->created_at)) }}</a>
+				<a href="/user/{{ $comment->user->username }}" title="{{$comment->user->username}}'s Profile">{{ $comment->user->username }}</a> says:
+			</div>
+		<div class="message">
+			<p>{{ $comment->html }}</p>
+		</div>
+		</div>
+	</li>
 	@endforeach
+</ul>
+<div id="respond">
 	@if(Auth::check())
 		<h4>Leave a comment</h4>
 		{{ Form::open("news/comment/".$article->id) }}
 		{{ Form::token() }}
-		{{ Form::field("textarea", "comment", "Comment", array(Input::old("comment"), array("class" => "input-xxlarge")), array('error' => $errors->first('comment'))) }}
+		{{ Form::field("textarea", "comment", "", array(Input::old("comment"), array("class" => "input-xxlarge")), array('error' => $errors->first('comment'))) }}
 		{{ Form::submit("Comment", array("class" => "btn-primary")) }}
 		{{ Form::close() }}
 	@else
 	You must be logged in to leave a comment.
 	@endif
-</article>
+</div>
+</div>
+</div>
 @include("news.sidebar")	
 </div>
 @endsection
