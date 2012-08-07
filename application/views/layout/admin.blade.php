05f796e57dc1b725c3643326ad5972c738108fa8
LeonimuZ <leontaveras@gmail.com>
2012-08-07 13:20:39 -0400

    menu item active if on the page

--- application/views/layout/admin.blade.php
+++ application/views/layout/admin.blade.php
@@ -3,22 +3,14 @@
 @section("content")
 	<nav id="pagemenu">
 		<ul class="nav nav-tabs">
-			<li>{{ HTML::link("admin", "Admin Home") }}</li>
-			<li>{{ HTML::link("admin/news", "News") }}</li>
-			<li>{{ HTML::link("admin/tournaments", "Tournaments") }}</li>
-			<li>{{ HTML::link("admin/maps", "Maps") }}</li>
-			<li>{{ HTML::link("admin/teams", "Teams") }}</li>
-			<li>{{ HTML::link("admin/pages", "Pages") }}</li>
-			<li>{{ HTML::link("admin/user", "Users") }}</li> 
-			<li class="dropdown">
-    		<a class="dropdown-toggle" data-toggle="dropdown" href="#">Comments
-        	<b class="caret"></b>
-      		</a>
-    		<ul class="dropdown-menu">
-      		<li>{{ HTML::link("admin/news/comments", "News") }}</li>
-      		<li>{{ HTML::link("admin/maps/comments", "Maps") }}</li>
-    		</ul>
-  			</li>
+			<li {{ URI::is('admin') ? 'class="active"' : '' }}>{{ HTML::link("admin", "Admin Home") }}</li>
+			<li {{ URI::is('admin/news') ? 'class="active"' : '' }}>{{ HTML::link("admin/news", "News") }}</li>
+			<li {{ URI::is('admin/tournaments') ? 'class="active"' : '' }}>{{ HTML::link("admin/tournaments", "Tournaments") }}</li>
+			<li {{ URI::is('admin/maps') ? 'class="active"' : '' }}>{{ HTML::link("admin/maps", "Maps") }}</li>
+			<li {{ URI::is('admin/teams') ? 'class="active"' : '' }}>{{ HTML::link("admin/teams", "Teams") }}</li>
+			<li {{ URI::is('admin/pages') ? 'class="active"' : '' }}>{{ HTML::link("admin/pages", "Pages") }}</li>
+			<li {{ URI::is('admin/user') ? 'class="active"' : '' }}>{{ HTML::link("admin/user", "Users") }}</li> 
+			<li {{ URI::is('admin/comments*') ? 'class="active"' : '' }}>{{ HTML::link("admin/comments", "Comments") }}</li>
 			<li><a onClick="MLM.images.open(); return false;" href="#" >Images</a></li>
 		</ul>
 	</nav>
--- application/views/user/menu.blade.php
+++ application/views/user/menu.blade.php
@@ -1,6 +1,15 @@
 <nav id="pagemenu">
 	<ul class="nav nav-tabs">
-		<li>{{ HTML::link('user', 'Your Profile'); }}</li>
-		<li>{{ HTML::link("account", "Edit Account") }}</li> 
+		<li {{ URI::is('user*') ? 'class="active"' : '' }}>{{ HTML::link(("user/".Auth::user()->username), "Profile"); }}</li>
+		<li {{ URI::is('messages*') ? 'class="dropdown active"' : 'dropdown' }}>
+    		<a class="dropdown-toggle" data-toggle="dropdown" href="#">Messages
+        	<b class="caret"></b>
+      		</a>
+    		<ul class="dropdown-menu">
+      		<li>{{ HTML::link("messages", "Inbox") }}</li>
+      		<li>{{ HTML::link("messages/new", "New") }}</li>
+    		</ul>
+  			</li>
+		<li {{ URI::is('account') ? 'class="rside active"' : 'class="rside"' }}>{{ HTML::link("account", "Edit Account") }}</li> 
 	</ul>
 </nav>
