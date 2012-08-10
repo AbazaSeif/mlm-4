b47e6c4e8688e0860e1d2f827e456e9bfd05c13e
LeonimuZ <leontaveras@gmail.com>
2012-08-10 15:34:02 -0400

    New slideshow for featured content
    In development

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -2,24 +2,42 @@
 
 @section('content')
 <div id="home" class="clearfix">
-<div id="slider">
-		<a href="#" data-caption="#caption1" ><img src="{{ URL::to_asset("images/slides/1.jpg") }}"></a>
-		<a href="#" data-caption="#caption2" ><img src="{{ URL::to_asset("images/slides/2.jpg") }}"></a>
-		<a href="#" data-caption="#caption3" ><img src="{{ URL::to_asset("images/slides/3.jpg") }}"></a>
-		<a href="#" data-caption="#caption4" ><img src="{{ URL::to_asset("images/slides/4.jpg") }}"></a>
-		<a href="#" data-caption="#caption5" ><img src="{{ URL::to_asset("images/slides/5.jpg") }}"></a>
-		<a href="#" data-caption="#caption6" ><img src="{{ URL::to_asset("images/slides/6.jpg") }}" ></a>
-		</div>
-		
-		<!-- Captions  -->
-		<span class="orbit-caption" id="caption1">Caption 1</span>
-		<span class="orbit-caption" id="caption2">Caption 2</span>
-		<span class="orbit-caption" id="caption3">Caption 3</span>
-		<span class="orbit-caption" id="caption4">Caption 4</span>
-		<span class="orbit-caption" id="caption5">Caption 5</span>
-		<span class="orbit-caption" id="caption6">Caption 6</span>	
 
-<div class="featured"></div>
+<div id="iview">
+			<div data-iview:image="{{ URL::to_asset("images/slides/1.jpg") }}" data-iview:transition="slice-top-fade,slice-right-fade">
+				<div class="iview-caption caption1" data-x="80" data-y="200">iView<sup>&trade;</sup></div>
+				<div class="iview-caption" data-x="60" data-y="255" data-transition="wipeRight">The world's most awesome jQuery Image & Content Slider</div>
+				<div class="iview-caption" data-x="234" data-y="300" data-transition="wipeLeft"><i>Presented by <b>Hemn Chawroka</b></i></div>
+			</div>
+
+			<div data-iview:image="{{ URL::to_asset("images/slides/2.jpg") }}" data-iview:transition="zigzag-drop-top,zigzag-drop-bottom" data-iview:pausetime="3000">
+				<div class="iview-caption caption5" data-x="40" data-y="260" data-transition="wipeDown">Captions can be positioned and resized freely</div>
+				<div class="iview-caption caption6" data-x="280" data-y="330" data-transition="wipeUp"><a href="#">Example URL-link</a></div>
+			</div>
+
+			<div data-iview:image="{{ URL::to_asset("images/slides/3.jpg") }}" data-iview:transition="zigzag-drop-top,zigzag-drop-bottom" data-iview:pausetime="3000">
+				<div class="iview-caption caption5" data-x="40" data-y="260" data-transition="wipeDown">Captions can be positioned and resized freely</div>
+				<div class="iview-caption caption6" data-x="280" data-y="330" data-transition="wipeUp"><a href="#">Example URL-link</a></div>
+			</div>
+
+			<div data-iview:image="{{ URL::to_asset("images/slides/4.jpg") }}">
+				<div class="iview-caption caption4" data-x="50" data-y="80" data-width="312" data-transition="fade">Some of iView's Options:</div>
+				<div class="iview-caption blackcaption" data-x="50" data-y="135" data-transition="wipeLeft" data-easing="easeInOutElastic">Touch swipe for iOS and Android devices</div>
+				<div class="iview-caption blackcaption" data-x="50" data-y="172" data-transition="wipeLeft" data-easing="easeInOutElastic">Image And Thumbs Fully Resizable</div>
+				<div class="iview-caption blackcaption" data-x="50" data-y="209" data-transition="wipeLeft" data-easing="easeInOutElastic">Customizable Transition Effect</div>
+				<div class="iview-caption blackcaption" data-x="50" data-y="246" data-transition="wipeLeft" data-easing="easeInOutElastic">Freely Positionable and Stylable Captions</div>
+				<div class="iview-caption blackcaption" data-x="50" data-y="283" data-transition="wipeLeft" data-easing="easeInOutElastic">Cross Browser Compatibility!</div>
+			</div>
+
+			<div data-iview:image="{{ URL::to_asset("images/slides/5.jpg") }}">
+				<div class="iview-caption caption7" data-x="0" data-y="0" data-width="160" data-height="460" data-transition="wipeRight"><h3>The Responsive Caption</h3>This is the product that you <b><i>all have been waiting for</b></i>!<br><br>Customize this slider with just a little HTML and CSS to your very needs. Give each slider some captions to transport your message.<br><br>All in all it works on every browser (including IE6 / 7 / 8) and on iOS and Android devices!</div>
+			</div>
+
+			<div data-iview:image="{{ URL::to_asset("images/slides/6.jpg") }}">
+				<div class="iview-caption caption5" data-x="40" data-y="130" data-transition="wipeLeft">What are you waiting for?</div>
+				<div class="iview-caption caption6" data-x="140" data-y="210" data-transition="wipeRight">Get it Now!</div>
+			</div>
+		</div>
 
 <?php /*
 <div id="ticker">
--- public/js/script.js
+++ public/js/script.js
@@ -8,23 +8,12 @@ MLM = {
 	},
 	home: {
 		init: function () {
-		$('#slider').orbit({
-				animation: 'horizontal-push',             // fade, horizontal-slide, vertical-slide, horizontal-push
-				animationSpeed: 800,                // how fast animtions are
-				timer: true, 			 // true or false to have the timer
-				advanceSpeed: 5000, 		 // if timer is enabled, time between transitions 
-				pauseOnHover: true, 		 // if you hover pauses the slider
-				startClockOnMouseOut: true, 	 // if clock should start on MouseOut
-				startClockOnMouseOutAfter: 300, 	 // how long after MouseOut should the timer start again
-				directionalNav: true, 		 // manual advancing directional navs
-				captions: true, 			 // do you want captions?
-				captionAnimation: 'slideOpen', 		 // fade, slideOpen, none
-				captionAnimationSpeed: 800, 	 // if so how quickly should they animate in
-				bullets: false,			 // true or false to activate the bullet navigation
-				bulletThumbs: false,		 // thumbnails for the bullets
-				bulletThumbLocation: '',		 // location from this file where thumbs will be
-				//afterSlideChange: function(){} 	 // empty function 
-			});
+		$('#iview').iView({
+					pauseTime: 7000,
+					directionNav: false,
+					controlNav: true,
+					tooltipY: -15
+				});
 		}
 	},
 	admin: {
