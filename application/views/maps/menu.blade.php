7633c758dd9214c0dea01373e2da35ed0ee36583
Ed Hanton <edh649@gmail.com>
2012-08-28 22:02:50 +0100

    manual merge xD

--- application/views/maps/menu.blade.php
+++ application/views/maps/menu.blade.php
@@ -1,20 +1,20 @@
 <nav id="pagemenu">
 	<ul class="nav nav-tabs">
-		<li {{ (Input::get('order') == 'newest') ? 'class="activated"' : 'class=""'}}>{{HTML::link("maps/filter?order=newest", "Newest") }}</li>
-		<li {{ (Input::get('featured') == 'true') ? 'class="activated"' : 'class=""'}}>{{ HTML::link('maps/filter?featured=true', 'Featured'); }}</li>
-		<li {{ (Input::get('order') == 'best') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter?order=best", "Highest ranked") }}</li>
-		<li {{ (Input::get('official') == 'true') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter?official=true", "Official Maps") }}</li>
+		<li>{{ HTML::link("account", "Newest") }}</li>
+		<li>{{ HTML::link('user', 'Featured'); }}</li>
+		<li>{{ HTML::link("account", "Highest ranked") }}</li>
+		<li>{{ HTML::link("account", "Official Maps") }}</li>
 		<li class="dropdown">
     		<a class="dropdown-toggle" data-toggle="dropdown" href="#">Categories
         	<b class="caret"></b>
       		</a>
     		<ul class="dropdown-menu">
-    		<li {{ (Input::get('type') == 'rtw') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter/?type=rtw", "Race For Wool") }} </li>
-      		<li {{ (Input::get('type') == 'ctq') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter/?type=ctq", "Capture the Wool") }}</li>
-      		<li {{ (Input::get('type') == 'dtc') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter/?type=dtc", "Destroy the Core") }}</li>
-      		<li {{ (Input::get('type') == 'att') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter/?type=att", "Attack/Defence") }}</li>
-      		<li {{ (Input::get('type') == 'bed') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter/?type=bed", "Bed Wars") }}</li>
-      		<li {{ (Input::get('type') == 'oth') ? 'class="activated"' : 'class=""'}}>{{ HTML::link("maps/filter/?type=oth", "Other") }}</li>
+    		<li>{{ HTML::link("maps/?rtw", "Race For Wool") }}</li>
+      		<li>{{ HTML::link("maps/?ctq", "Capture the Wool") }}</li>
+      		<li>{{ HTML::link("maps/?dtc", "Destroy the Core") }}</li>
+      		<li>{{ HTML::link("maps/?att", "Attack/Defence") }}</li>
+      		<li>{{ HTML::link("maps/?bed", "Bed Wars") }}</li>
+      		<li>{{ HTML::link("maps/?oth", "Other") }}</li>
     		</ul>
   		</li>
 		@if (Auth::check())
