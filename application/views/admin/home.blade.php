73a209b0e229368710572fe5e8d18e9b66cc4a0b
t2t2 <taavot@gmail.com>
2012-09-02 21:40:07 +0300

    Admin log display

--- application/views/admin/home.blade.php
+++ application/views/admin/home.blade.php
@@ -2,9 +2,27 @@
 
 @section('content')
 @parent
-<div id="content">
-	<div class="titlebar clearfix">
-		<h2>STATS, AND MODERATION LOG WILL GO HERE<br>AND THIS IS A TEST</h2>
+<div id="content" class="clearfix">
+	<div id="page">
+		<div class="titlebar clearfix">
+			<h2>Moderation queue</h2>
+		</div>
+	</div>
+	<div id="sidebar">
+		<div class="titlebar clearfix">
+			<h2>Admin log</h2>
+		</div>
+		<ol>
+			@foreach($log->results as $logitem)
+				<li>
+					{{$logitem->user->username}} {{$logitem->action}} {{$logitem->module}} item ID#{{$logitem->target}}
+					@if($logitem->text)
+					<br />{{$logitem->text}}
+					@endif
+				</li>
+			@endforeach
+		</ol>
+		{{ $log->links() }}
 	</div>
 </div>
 @endsection
