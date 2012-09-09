38a203bfab574b7c74496fe0d9f308a4812b1d91
LeonimuZ <leontaveras@gmail.com>
2012-09-09 15:55:50 -0400

    Viewport

--- application/views/layout/main.blade.php
+++ application/views/layout/main.blade.php
@@ -8,6 +8,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	@if (isset($title)) 
 	<title>{{$title}} &raquo; MLM</title>
 	@else 
