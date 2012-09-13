c00b8bd4c1471f20dff7eba92ca87bbe4a268af0
LeonimuZ <leontaveras@gmail.com>
2012-09-13 17:14:38 -0400

    Updated, fixed & redesigned comments
    Needs to be configured, needs to be on its on page and that page can be
    called from anywhere on the site (much less coding/styling)...  to save
    you time check out the comment_reply_link function on Wordpress @t2t2

--- application/views/news/view.blade.php
+++ application/views/news/view.blade.php
@@ -32,20 +32,32 @@
 	<div class="titlebar clearfix"><h2>No Comments</h2></div>
 	@else
 	<div class="titlebar clearfix"><h2><b>{{ $article->comment_count }}</b> Comments</h2></div>
+	<div class="showcomments" style="display:none;"></div>
 	@endif
 
 <ol class="commentlist">
 	@foreach($article->comments as $comment)
-	<li class="clearfix">
+	<li>
 		<div id="comment{{$comment->id}}">
-			<div class="comment-author vcard avatar-cell"> 
-			<img class="avatar" src="http://minotar.net/helm/{{$comment->user->mc_username}}/64.png" alt="{{ $comment->user->username }}'s skin" title="{{ $comment->user->username }}'s skin">
+			<div class="vcard"> 
+			<img class="avatar" src="http://minotar.net/helm/{{$comment->user->mc_username}}/150.png" alt="{{ $comment->user->username }}'s skin" title="{{ $comment->user->username }}'s skin">
+			<a class="username" href="/user/{{ $comment->user->username }}" title="{{$comment->user->username}}'s Profile">{{ $comment->user->username }}</a> Says:
 			</div>
-			<div class="comment-meta commentmetadata comment_text clearfix">
-				<div style="float: right;" class="comment_date"><a href="{{ URL::to_action("news@view", array($article->id, $article->slug)) }}#comment{{$comment->id}}">{{ date("M j,Y g:ia", strtotime($comment->created_at)) }}</a></div>
-				<div class="comment_author"><a href="/user/{{ $comment->user->username }}" title="{{$comment->user->username}}'s Profile">{{ $comment->user->username }}</a></div>
-				<div class="comment-body"><p>{{ $comment->html }}</p></div>
+		<div class="body">
+			{{ $comment->html }}
+		</div>
+		<div class="meta">
+			<span class="time"><a href="{{ URL::to_action("news@view", array($article->id, $article->slug)) }}#comment{{$comment->id}}">{{ date("M j,Y g:ia", strtotime($comment->created_at)) }}</a></span>
+			<div class="actions">
+			<span><a href="#"><i class="icon-share-alt"></i> Reply</a></span> {{-- Supposed to bring the comment imput up and add comment under parent --}}
+			<span><a href="#"><i class="icon-arrow-up"></i> Upvote</a></span> {{-- Adds a star somewhere in the comment if it has more than 10 upvotes --}}
+			<span><a href="#"><i class="icon-flag"></i> Report</a></span> {{-- this is a popup with a dropdown with reasons and a little imput field --}}
+			@if (Auth::user() && Auth::user()->admin)
+			<span> | <a href="#" onclick="return false"><i class="icon-cog"></i> Moderate</a></span> {{-- Delete comment, Message user, Ban user --}}
+			@else
+			@endif
 			</div>
+		</div>
 		</div>
 	</li>
 	@endforeach
@@ -53,14 +65,15 @@
 
 	<div id="respond">
 	@if(Auth::check())
-		<h4>Leave a comment</h4>
+		<h3>Leave a comment</h3>
 		{{ Form::open("news/comment/".$article->id) }}
 		{{ Form::token() }}
 		{{ Form::field("textarea", "comment", "", array(Input::old("comment"), array("class" => "input-xxlarge")), array('error' => $errors->first('comment'))) }}
 		{{ Form::submit("Comment", array("class" => "btn-primary")) }}
+		{{ HTML::link('#', 'Preview', array("class" => "btn", "onclick" => "alert('Hello World!'); return false")); }}
 		{{ Form::close() }}
 	@else
-	You must be <a href="/login">logged in</a> to leave a comment.
+	<h4>You must be <a href="/login">logged in</a> to leave a comment.</h4>
 	@endif
 	</div>
 </div>
