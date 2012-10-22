7177c786cff328d3e1d9623e502acf6345aa9ce6
Ed Hanton <edh649@gmail.com>
2012-10-23 00:22:28 +0200

    Fixing Search
    Fixed search
    (note: to use this fix, you must have cleaner urls enabled (i.e., 'index.php' is not in the url)

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -13,14 +13,14 @@ class Base_Controller extends Controller {
 	{
 		return Response::error('404');
 	}
-/*
+	
 	public function __construct()
-    {
-    	// Generate a 'moduleURL' variable so we can access which module we are currently i
-    	// (Used primarily in search)
-    	$url = explode('/', URL::current());
-    	$moduleurl = $url[4];
-    	View::share('moduleURL', $moduleurl);
-    }
-*/
+    	{
+	    	// Generate a 'moduleURL' variable so we can access which module we are currently i
+	    	// (Used primarily in search)
+	    	$url = explode('/', URL::current());
+	    	$moduleurl = $url[3];
+	    	View::share('moduleURL', $moduleurl);
+    	}
+    
 }
