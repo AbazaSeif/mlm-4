ecb7bf77d2c3fe13e447c637c70dc9ff5a6b236c
t2t2 <taavot@gmail.com>
2012-10-22 09:54:15 +0300

    New message: Specifying who to using ?to=username in URL

--- application/views/messages/new.blade.php
+++ application/views/messages/new.blade.php
@@ -14,7 +14,7 @@
 				<div class="titlebar"><h3>Subject</h3></div>
 				{{ Form::field("text", "title", "", array(Input::old("title"), array('class' => 'title')), array('error' => $errors->first('title'))) }}
 				<div class="titlebar"><h3>Send to <small>(Separete multiple users with a comma)</small></h3></div>
-					{{ Form::field("text", "users", "", array(Input::old("users"), array('class' => 'title')), array('error' => $errors->first('users'))) }}
+					{{ Form::field("text", "users", "", array(Input::old("users", Input::get("to")), array('class' => 'title')), array('error' => $errors->first('users'))) }}
 				<div class="titlebar"><h3>Message</h3></div>
 					{{ Form::field("textarea", "message", "", array(Input::old("message"), array("id" => "mrk", 'class' => 'summary')), array('error' => $errors->first('message'))) }}
 					{{ Form::actions(array( Form::submit("Send", array("class" => "btn-primary")), " ", HTML::link("messages", "Back", array("class" => "btn")) )) }} 
