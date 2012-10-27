93962677b097c37aafabb536de150f9ff78b0bb0
LeonimuZ <leontaveras@gmail.com>
2012-10-27 03:40:06 -0400

    New profile page 70%
    Still need to configure the tabs to do stuff.

--- application/views/user/home.blade.php
+++ application/views/user/home.blade.php
@@ -16,13 +16,14 @@
 	</div>
 	<div class="name">
 		<h1>{{$user->username}}</h1>
-		<h2><img src="{{ URL::to_asset("images/static/mc-icon.png") }}">{{$user->mc_username}}</h2>
+		<h2><img src="{{ URL::to_asset("images/static/mc-icon.png") }}" title="Minecraft Username">{{$user->mc_username}}</h2>
 		<h3>Joined {{ date("F j, Y", strtotime($user->created_at)) }}</h3>
 	</div>
 	<div class="stats">
 		<ul>
 			<li><a href="#"><span>{{ $user->comment_count }}</span>Comments</a></li>
-			<li><a href="#"><span>456</span>Maps</a></li>
+			<?php $uid = $user->id ?>
+			<li><a href="#"><span>{{ DB::table("map_user")->where_user_id($uid)->count(); }}</span>Maps</a></li>
 			<li><a href="#"><span>789</span>Wins</a></li>
 			<li><a href="#"><span>012</span>Loses</a></li>
 			<li><a href="#"><span>345</span>Ranking</a></li>
@@ -36,67 +37,54 @@
 			@elseif ($user->rank == 2)<div class="user-rank editor" title="MLM Editor"></div>
 			@elseif ($user->rank == 1)<div class="user-rank mod" title="MLM Moderator"></div>@endif
 			</li>
-			<li><a href="#" class="nm"><i class="icon-envelope-alt"></i></a></li>
-			<li></li>
+			<li><a href="{{ URL::to("messages/new") }}" title="Messages" data-value="Message {{$user->username}}"><i class="icon-envelope-alt"></i></a></li>
+			@if ($user->profile->reddit)
+			<li><a href="http://reddit.com/user/{{$user->profile->reddit}}" target="_blank" rel="nofollow" title="reddit" data-value="{{$user->profile->reddit}}"><i class="icon-arrow-up"></i></a></li>
+			@endif
+			@if ($user->profile->twitter)<li><a href="http://twitter.com/{{$user->profile->twitter}}" target="_blank" rel="nofollow" title="Twitter" data-value="{{$user->profile->twitter}}"><i class="icon-twitter"></i></a></li>
+			@endif
+			@if ($user->profile->youtube)
+			<li><a href="http://youtube.com/user/{{$user->profile->youtube}}" target="_blank" rel="nofollow" title="YouTube" data-value="{{$user->profile->youtube}}"><i class="icon-play"></i></a></li>
+			@endif
+			@if ($user->profile->webzone)
+			<li><a href="{{$user->profile->webzone}}" target="_blank" rel="nofollow" title="Website" data-value="{{$user->profile->webzone}}"><i class="icon-globe"></i></a></li>
+			@endif
 		</ul>
 	</div>
 </header>
-	<div id="page">
-		<div id="feed">
-			<div class="titlebar">
-				<h2>Feed</h2>
-			</div>
-		</div>
-	</div>
-	<aside id="sidebar">
-		<div class="widget teaminfo">
-			<header><h1>Team</h1></header>
-		<div class="content">
-			<a href="#"><img src="http://placekitten.com/100/100" />
-				<div class="data">
-					<p>The Quick Brown Fox Jumps Over The Lazy Dog</p>
-					<ul class="numbers">
-						<li>Wins<strong>888</strong></li>
-						<li>Loses<strong>888</strong></li>
-					</ul>
-				</div>
-			</a>
-		</div>
-		</div>
-			<div class="widget">
-				<header><h1>{{$user->username}}'s Info</h1></header>
-			<div class="content">
-			<div class="info">
-		<ul>
-		@if ($user->profile->reddit)
-		<li>
-		<label>Reddit</label>
-		<p><a href="http://reddit.com/user/{{$user->profile->reddit}}" target="_blank" rel="nofollow">{{$user->profile->reddit}}</a></p>
-		</li>
-		@endif
-		@if ($user->profile->twitter)
-		<li>
-		<label>Twitter</label>
-		<p><a href="http://twitter.com/{{$user->profile->twitter}}" target="_blank" rel="nofollow">{{$user->profile->twitter}}</a></p>
-		</li>
-		@endif
-		@if ($user->profile->youtube)
-		<li>
-		<label>YouTube</label>
-		<p><a href="http://youtube.com/user/{{$user->profile->youtube}}" target="_blank" rel="nofollow">{{$user->profile->youtube}}</a></p>
-		</li>
-		@endif
-		<li>
-		<label>Member since</label>
-		<p>{{ date("F j, Y", strtotime($user->created_at)) }}</p>
-		</li>
-		<li>
-		<label>Last updated</label>
-		<p>{{ date("F j, Y", strtotime($user->updated_at)) }}</p>
-		</li>
+<script type="text/javascript">
+$('#myTab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+})
+</script>
+	<div id="page" class="maxwidth">
+		<ul id="myTab" class="nav nav-tabs">
+			<li class=""><a href="#home" data-toggle="tab">Home</a></li>
+			<li class=""><a href="#profile" data-toggle="tab">Profile</a></li>
+			<li class="dropdown active">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li class="active"><a href="#dropdown1" data-toggle="tab">@fat</a></li>
+                  <li><a href="#dropdown2" data-toggle="tab">@mdo</a></li>
+                </ul>
+			</li>
 		</ul>
-			</div>
-			</div>
-	</aside>
+		<div id="myTabContent" class="tab-content">
+              <div class="tab-pane fade" id="home">
+                <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+              </div>
+              <div class="tab-pane fade" id="profile">
+                <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
+              </div>
+              <div class="tab-pane fade active in" id="dropdown1">
+                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
+              </div>
+              <div class="tab-pane fade" id="dropdown2">
+                <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral, mustache readymade thundercats keffiyeh craft beer marfa ethical. Wolf salvia freegan, sartorial keffiyeh echo park vegan.</p>
+              </div>
+            </div>
+	</div>
+	</div>
 </div>
 @endsection
