659eb736a28070188c118349ab734c7bcd95a1b2
LeonimuZ <leontaveras@gmail.com>
2012-10-30 04:30:51 -0400

    Remove matches
    they came with a cherry-pick

--- application/views/matches/menu.blade.php
+++
@@ -1,9 +0,0 @@
-<nav id="pagemenu">
-	<ul class="nav nav-tabs">
-		<li>{{ HTML::link('/matches', 'Match Home'); }}</li>
-		<li>{{ HTML::link("matches/list", "Match List") }}</li>
-		@if (Auth::check())
-		<li {{ URI::is('matches/new') ? 'class="rside active"' : 'class="rside btn-info"' }}>{{ HTML::link("matches/new", "New Match") }}</li>
-		@endif
-	</ul>
-</nav>
