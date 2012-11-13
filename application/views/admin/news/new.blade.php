4575cb95a0985dec0f1b3e0d687f856103c46cc2
LeonimuZ <leontaveras@gmail.com>
2012-11-12 22:59:14 -0500

    Added Wysywig to some admin pages

--- application/views/admin/news/new.blade.php
+++ application/views/admin/news/new.blade.php
@@ -3,19 +3,17 @@
 @section("content")
 @include("admin.menu")
 <div id="content" class="edit clearfix">
-<div class="titlebar">
-	<h2>New article</h2>
-</div>
+<div class="titlebar"><h2>New article</h2></div>
 <div id="page" class="maxwidth">
-	{{ Form::open(null, 'POST', array('class' => 'form')) }}
-		{{ Form::token()}}
-		{{ Form::field("text", "title", "", array(Input::old("title"), array('class' => 'title', 'autocomplete' => 'off', 'placeholder' => 'Article Title')), array('error' => $errors->first('title'))) }}
-		{{ Form::field("imageselect", "image", "Article main image", array(Input::old("image"), $oldimage), array("alt" => "(Use default if no image is found)", "error" => $errors->first("image"))) }}
-		{{ Form::field("wysiwyg", "news_content", "Article content", array(Input::old("news_content"), array('class' => 'input-xxlarge')), array('error' => $errors->first('news_content'))) }}
-		{{ Form::field("textarea", "summary", "Summary of the Article", array(Input::old("summary"), array("rows" => "15", 'class' => 'summary')), array('alt' => '(250 characters max)', 'error' => $errors->first('summary'))) }}
-		{{ Form::field("checkbox", "published", "Would you like to publish this article right away?", array(1, Input::old("published")), array("error" => $errors->first("published"))) }}
-		{{ Form::actions(array(Form::submit("Save", array("class" => "btn-primary"))," ",HTML::link_to_action("admin@news", "Cancel",array(), array("class" => "btn") ))) }}
-	{{ Form::close() }}
+{{ Form::open(null, 'POST', array('class' => 'form')) }}
+	{{ Form::token()}}
+	{{ Form::field("text", "title", "", array(Input::old("title"), array('class' => 'title', 'autocomplete' => 'off', 'placeholder' => 'Article Title')), array('error' => $errors->first('title'))) }}
+	{{ Form::field("imageselect", "image", "Article main image", array(Input::old("image"), $oldimage), array("alt" => "(Use default if no image is found)", "error" => $errors->first("image"))) }}
+	{{ Form::field("wysiwyg", "news_content", "Article content", array(Input::old("news_content"), array('class' => 'input-xxlarge')), array('error' => $errors->first('news_content'))) }}
+	{{ Form::field("textarea", "summary", "Summary of the Article", array(Input::old("summary"), array("rows" => "15", 'class' => 'summary')), array('alt' => '(250 characters max)', 'error' => $errors->first('summary'))) }}
+	{{ Form::field("checkbox", "published", "Would you like to publish this article right away?", array(1, Input::old("published")), array("error" => $errors->first("published"))) }}
+	{{ Form::actions(array(Form::submit("Save", array("class" => "btn-primary"))," ",HTML::link_to_action("admin@news", "Cancel",array(), array("class" => "btn") ))) }}
+{{ Form::close() }}
 </div>
 </div>
 @endsection
--- application/views/admin/pages/edit.blade.php
+++ application/views/admin/pages/edit.blade.php
@@ -2,20 +2,14 @@
 
 @section("content")
 @include("admin.menu")
-<div id="content">
-<div class="titlebar">
-	<h2>Editing page <b>{{$page->title}}</b></h2>
-</div>
-{{ Form::open(null , 'POST', array('class' => 'form-horizontal')) }} 
-	<fieldset>
-		{{ Form::token() }}
-
-		{{ Form::field("text", "title", "Title", array(Input::old("title", $page->title), array('class' => 'input-xxlarge','autocomplete' => 'off')), array('error' => $errors->first('title'))) }}
-		{{ Form::field("text", "slug", "Slug", array(Input::old("slug", $page->url_slug), array('class' => 'input-large')), array("error" => $errors->first("slug"))) }}
-		{{ Form::field("wysiwyg", "data", "Data", array(Input::old("data", $page->data), array("rows" => "15", 'class' => 'input-xxlarge')), array("error" => $errors->first("data"))) }}		
-
-		{{ Form::actions(array( Form::submit("Save", array("class" => "btn btn-primary")) )) }}
-	</fieldset>
+<div id="content" class="edit">
+<div class="titlebar"><h2>Editing page <b>{{$page->title}}</b></h2></div>
+{{ Form::open(null, 'POST', array('class' => 'form')) }}
+	{{ Form::token()}}
+	{{ Form::field("text", "title", "", array(Input::old("title", $page->title), array('class' => 'title', 'autocomplete' => 'off', 'placeholder' => 'Page Title')), array('error' => $errors->first('title'))) }}
+	{{ Form::field("text", "slug", "Slug", array(Input::old("slug", $page->url_slug), array('class' => 'subtitle')), array("help" => "The page's URL. You don't need to put the whole URL just the slug, example: majorleaguemining.net/[Slug]", 'error' => $errors->first('slug'))) }}
+	{{ Form::field("wysiwyg", "data", "Content", array(Input::old("data", $page->data), array('class' => 'input-xxlarge')), array('error' => $errors->first('data'))) }}
+	{{ Form::actions(array(Form::submit("Save", array("class" => "btn btn-primary"))," ",HTML::link_to_action("admin@pages", "Cancel",array(), array("class" => "btn") ))) }}
 {{ Form::close() }}
 </div>
 @endsection
