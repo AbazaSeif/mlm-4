58f2768a241f74241fe61c66ec5cf69469b6344a
LeonimuZ <leontaveras@gmail.com>
2012-11-12 22:59:14 -0500

    Added Wysywig to some admin pages

--- application/views/admin/faq/form.blade.php
+++ application/views/admin/faq/form.blade.php
@@ -2,18 +2,13 @@
 
 @section("content")
 @include("admin.menu")
-<div id="content">
-<div class="titlebar">
-	<h2>Editing FAQ <b>{{$faq->title}}</b></h2>
-</div>
-{{ Form::open(null, 'POST', array('class' => 'form-horizontal')) }}
-	{{ Form::token() }}
-	<formset>
-		{{ Form::field("text", "question", "Question", array(Input::old("question", $faq->question), array('class' => 'input-large')), array('error' => $errors->first('question'))) }}
-		{{ Form::field("textarea", "answer", "Answer", array(Input::old("answer", $faq->answer), array('class' => 'input-large')), array('error' => $errors->first('answer'))) }}
-		
-		{{ Form::actions(array( Form::submit("Save", array("class" => "btn btn-primary")) )) }}
-	</formset>
+<div id="content" class="edit">
+<div class="titlebar"><h2>Editing FAQ question: <b>{{$faq->title}}</b></h2></div>
+{{ Form::open(null, 'POST', array('class' => 'form')) }}
+	{{ Form::token()}}
+	{{ Form::field("text", "question", "", array(Input::old("question",$faq->question), array('class' => 'title', 'autocomplete' => 'off', 'placeholder' => 'Question')), array('error' => $errors->first('question'))) }}
+	{{ Form::field("wysiwyg", "answer", "Answer", array(Input::old("answer",$faq->answer), array('class' => 'input-xxlarge')), array('error' => $errors->first('answer'))) }}
+	{{ Form::actions(array(Form::submit("Save", array("class" => "btn btn-primary"))," ",HTML::link_to_action("admin@faq", "Cancel",array(), array("class" => "btn") ))) }}
 {{ Form::close() }}
 </div>
 @endsection
--- application/views/admin/faq/new.blade.php
+++ application/views/admin/faq/new.blade.php
@@ -2,18 +2,13 @@
 
 @section("content")
 @include("admin.menu")
-<div id="content">
-<div class="titlebar">
-	<h2>Creating FAQ Question</h2>
-</div>
-{{ Form::open(null, 'POST', array('class' => 'form-horizontal')) }}
-	{{ Form::token() }}
-	<formset>
-		{{ Form::field("text", "question", "Question", array(Input::old("question"), array('class' => 'input-large')), array('error' => $errors->first('question'))) }}
-		{{ Form::field("textarea", "answer", "Answer", array(Input::old("answer"), array('class' => 'input-large')), array('error' => $errors->first('answer'))) }}
-		
-		{{ Form::actions(array( Form::submit("Create", array("class" => "btn btn-primary")) )) }}
-	</formset>
+<div id="content" class="edit">
+<div class="titlebar"><h2>Creating FAQ Question</h2></div>
+{{ Form::open(null, 'POST', array('class' => 'form')) }}
+	{{ Form::token()}}
+	{{ Form::field("text", "question", "", array(Input::old("question"), array('class' => 'title', 'autocomplete' => 'off', 'placeholder' => 'Question')), array('error' => $errors->first('question'))) }}
+	{{ Form::field("wysiwyg", "answer", "Answer", array(Input::old("answer"), array('class' => 'input-xxlarge')), array('error' => $errors->first('answer'))) }}
+	{{ Form::actions(array(Form::submit("Create", array("class" => "btn btn-primary"))," ",HTML::link_to_action("admin@faq", "Cancel",array(), array("class" => "btn") ))) }}
 {{ Form::close() }}
 </div>
 @endsection
