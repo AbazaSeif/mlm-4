7015ff04157754dd2348aa3d3e3c1fa4dc65fb28
t2t2 <taavot@gmail.com>
2013-01-02 02:34:22 +0200

    And another durrrr

--- application/views/maps/edit_version.blade.php
+++ application/views/maps/edit_version.blade.php
@@ -18,7 +18,7 @@
 	@endif
 		{{ Form::token() }}
 		{{ Form::field("text", "version", "Version", array(Input::old("version", $version->version)), array('error' => $errors->first('version'))) }}
-		{{ Form::field("textarea", "changelog", "Changelog & release notes", array(Input::old("changelog", $version->changelog), array("class" => "input-xxlarge")), array('error' => $errors->first('version'))) }}
+		{{ Form::field("textarea", "changelog", "Changelog & release notes", array(Input::old("changelog", $version->changelog), array("class" => "input-xxlarge")), array('error' => $errors->first('changelog'))) }}
 		{{ Form::field("file", "mapfile", "Map file (zip)", array(), array("help" => 'Check out <a href="http://majorleaguemining.net/mapmakerchecklist" target="_blank">The Mapmakers Checklist</a> for info on how to properly package your map.<br />Max file size 15MB. If your file is larger, contact us!'.($version->exists ? " If you'd like to keep the current file, don't uplaod anything." : ""), 'error' => $errors->first('mapfile'))) }}
 		{{ Form::actions(array(Form::submit("Save", array("class" => "btn-primary")), " ", HTML::link_to_action("maps@edit", "Back", array($map->id), array("class" => "btn")))) }}
 	{{ Form::close() }}
