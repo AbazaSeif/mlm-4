c182c87a050cc4d14c82982e5ddf90b96c390212
LeonimuZ <leontaveras@gmail.com>
2013-01-12 13:11:15 -0500

    Square Multiview view, updated Teams list.
    My last commit on this project. Thanks for the cake.

--- application/views/teams/list.blade.php
+++ application/views/teams/list.blade.php
@@ -3,18 +3,17 @@
 @section("content")
 @include("teams.menu")
 <div id="content">
-<div id="titlebar"></div>
+<div class="titlebar"><h2>TEAMS</h2></div>
 <div id="multiview">
-<ul class="list">
+<ul class="squared">
 @foreach ($teams->results as $team)
 	<li>
 	<a href="{{ URL::to_action("teams@view", array($team->id)) }}" title="View team">
+		<div class="mv-image">
+			<img src="<?php /* URL::to_asset("images/static/blank.gif") */ ?>http://placekitten.com/176/176" data-original="" alt="{{ $team->name }}" />
+		</div>
 		<div class="mv-details">
 		<div class="mv-title"><h1>{{ $team->name }}</div>
-		<div class="mv-summary"><p>{{ $team->tagline }}</div>
-		<div class="mv-meta">
-			<span>Members: <b>{{ count($team->users) }}</span>
-		</div>
 		</div>
 	</a>
 	</li>
